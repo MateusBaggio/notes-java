c7bb711c8dfb5f18a3b26df82921bfdd100fcbbd
Mateus Santos Baggio <167319374+MateusBaggio@users.noreply.github.com>
2024-10-15 20:03:02 -0300

    Create aulas.java

---
+++ aulas.java
@@ -0,0 +1,97 @@
+public class MyClass {
+  public static void main(String args[]) {
+   /*
+    int idade = 15 ;
+    String nome = "Mateus";
+    boolean homem = true;
+    char letra = 'M';
+    double altura = 1.80;
+    System.out.println(idade);
+    */
+
+    // Constantes variaveis
+
+      /*
+    final int idade_maxima = 100;
+    System.out.println(idade_maxima);
+    */
+
+    // Array
+    /*
+    String[] nome new String[5]; nome[0] "Guilherme"; 
+    nome [0] = "Mateus"
+    nome [1] = "João";
+    System.out.println(nome[0]);
+    */
+
+    // Array Multidimencional
+    /*
+    String[][] var2= new String[5][5];
+    var2[0][0] = "Felipe"; 
+    System.out.println(var2[0][0]);
+    */
+
+    // Condições
+    /*
+    int idade = 15;
+
+    if(idade < 18){
+      System.out.println("Você é menor de idade!");
+    }
+    else if(idade == 17){
+      System.out.println("Falta um ano para se tornar maior de idade")
+    }
+    else{
+      System.out.println("Você é maior de idade");
+    }
+    */
+
+    // Switch case
+    /*
+    int vida = 100;
+  
+    switch(vida){
+      case 90:
+        //Se for 90, execute:
+        System.out.println("Você já perdeu 10 de vida");
+          break;
+      case 30:
+        System.out.println("Você já perdeu 70 de vida");
+        break;
+        
+      default:
+        System.out.println("nenhuma condição bateu")
+    }
+    */
+
+    // Operadores lógicos e operações matemáticas.
+    /*
+      // operadores lógicos e, ou, not mesmo do javaScript.
+    int idade1 = 15;
+    int idade2 = 15;
+    int idade_final = 15 +15;
+    int idade_final2 = ((15 + 15)*2) /4;
+   */
+
+   // loopings
+    /*
+    int contador = 1;
+
+    while(contador <= 10) {
+    System.out.println("Olá, agora o contador está em: "+contador);
+    contador++;
+    }
+    
+    for(int i=1;i<=10;i++) {
+    System.out.println("Olá, agora o contador está em:"+1);
+    }
+    
+    contador = 0;
+    
+    do{
+    System.out.println(contador);
+    contador++;
+    }while(contador < 3);
+    */
+  }
+}
