db09921ca6ca8a432cd2af2a81dd192dca2e0a3b
Mateus Santos Baggio <167319374+MateusBaggio@users.noreply.github.com>
2024-10-15 21:31:56 -0300

    Create firstjavacode.java

---
+++ firstjavacode.java
@@ -0,0 +1,35 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		Scanner in = new Scanner(System.in);
+		String nome,idade,peso,estado,cidade,pais;
+		
+		System.out.println("Insira o seu nome");
+		nome = in.nextLine();
+		System.out.println("Insira a sua idade");
+		idade = in.nextLine();
+		System.out.println("Insira o seu peso");
+		peso = in.nextLine();
+		System.out.println("Insira o seu estado");
+		estado = in.nextLine();
+		System.out.println("Insira a sua cidade");
+		cidade = in.nextLine();
+		System.out.println("Insira o seu país");
+		pais = in.nextLine();
+		
+		System.out.println("------------------------------------------");
+		System.out.println("nome:" + nome);
+		System.out.println("idade :" + idade);
+		System.out.println("peso :" + peso);
+		System.out.println("estado :" + estado);
+		System.out.println("cidade :" + cidade);
+		System.out.println("país :" + pais);
+		
+		
+		
+
+		
+	}
+}
+
